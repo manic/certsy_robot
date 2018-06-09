0b1afad55add24f6b90279446f2f6a24e434a1dd
Manic Chuang <(none)>
2018-06-10 01:21:37 +0800

    feat(PLACE,REPORT): implement two functions

--- lib/certsy_robot.rb
+++ lib/certsy_robot.rb
@@ -1,4 +1,53 @@
 # frozen_string_literal: true
 
 class CertsyRobot
+  attr_reader :state, :x, :y, :direction
+  VALID_DIRECTIONS = %w[NORTH SOUTH EAST WEST]
+  VALID_ACTIONS = %w[PLACE MOVE LEFT RIGHT REPORT]
+  COMMAND_PATTERN = /(#{VALID_ACTIONS.join('|')})\s*(.+)?/
+
+  def initialize
+    @state = :pending
+  end
+
+  def command(command_string)
+    matched = command_string.match(COMMAND_PATTERN)
+    return unless matched
+    case matched[1]
+    when 'PLACE'
+      x, y, direction = matched[2].split(',')
+      place(x, y, direction)
+    when 'REPORT'
+      return if pending?
+      puts report
+    end
+  end
+
+  def report
+    "#{x},#{y},#{direction}"
+  end
+
+  private
+
+  def pending?
+    state == :pending
+  end
+
+  def valid_position?(x, y)
+    (0..5).include?(x.to_i) && (0..5).include?(y.to_i)
+  end
+
+  def valid_direction?(direction)
+    VALID_DIRECTIONS.include?(direction)
+  end
+
+  def place(x, y, direction)
+    return unless valid_position?(x, y) && valid_direction?(direction)
+    @direction = direction
+    @x = x.to_i
+    @y = y.to_i
+    @state = :placed
+  end
+
+  def ignore; nil end
 end
--- spec/certsy_robot_spec.rb
+++ spec/certsy_robot_spec.rb
@@ -2,5 +2,55 @@ require 'spec_helper'
 require 'certsy_robot'
 
 RSpec.describe CertsyRobot do
-  Then { CertsyRobot.new }
+  Given(:robot) { CertsyRobot.new }
+
+  context 'initial state: pending' do
+    Then { robot.state == :pending }
+  end
+
+  context '#command' do
+    context 'when receive PLACE' do
+      When { allow(robot).to receive(:place).and_call_original }
+      When { robot.command('PLACE 1,2,SOUTH') }
+      Then { expect(robot).to have_received(:place).with('1', '2', 'SOUTH') }
+      And { robot.state == :placed }
+      And { robot.x == 1 }
+      And { robot.y == 2 }
+      And { robot.direction == 'SOUTH' }
+    end
+
+    context 'when receive PLACE twice' do
+      When { robot.command('PLACE 1,2,NORTH') }
+      When { robot.command('PLACE 2,3,WEST') }
+      Then { robot.state == :placed }
+      And { robot.x == 2 }
+      And { robot.y == 3 }
+      And { robot.direction == 'WEST' }
+    end
+
+    context 'when receive invalid PLACE command' do
+      context 'when direction is invalid' do
+        When { robot.command('PLACE 1,3,INVALID') }
+        Then { robot.state == :pending }
+        And { robot.x == nil }
+        And { robot.y == nil }
+        And { robot.direction == nil }
+      end
+
+      context 'when position is invalid' do
+        When { robot.command('PLACE 1,9,NORTH') }
+        Then { robot.state == :pending }
+        And { robot.x == nil }
+        And { robot.y == nil }
+        And { robot.direction == nil }
+      end
+    end
+
+    context 'when receive REPORT' do
+      When { robot.command('PLACE 1,2,SOUTH') }
+      Then do
+        expect { robot.command('REPORT') }.to output("1,2,SOUTH\n").to_stdout
+      end
+    end
+  end
 end
